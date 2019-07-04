aca8f8904c4f19c9fa3113f7b0d25bc69b04d89d
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-07-04 10:35:45 -0700

    articles not posting to database :(, coming into console, but can't get them to populate in DB

--- models/Article.js
+++ models/Article.js
@@ -7,16 +7,16 @@ var ArticleSchema = new Schema({
     // `title` is required and of type String
     title: {
         type: String,
-        required: true,
     },
     // `link` is required and of type String
     link: {
         type: String,
-        required: true,
     },
     summary: {
-        type: String,
-        required: true,
+        type: String
+    },
+    image: {
+        type: String
     },
     // `note` is an object that stores a Note id
     // The ref property links the ObjectId to the Note model
--- server.js
+++ server.js
@@ -1,76 +1,112 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const logger = require("morgan")
-const path = require("path")
-const axios = require("axios")
-const cheerio = require("cheerio")
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
+app.use(bodyParser.json()); // Send JSON responses
+app.use(logger('dev'));
 
-app.use(logger("dev"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static('views'));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static("public"))
-app.use(express.static("views"))
+const exphbs = require('express-handlebars');
 
-const exphbs = require("express-handlebars")
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
+const db = require('./models');
 
-const db = require("./models")
-
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/BrWebscraper';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
 // Routes
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     db.Article.find({}).then(dbArticle => {
-        // console.log(dbArticle)
-        res.render("index", {articles: dbArticle});
+    // console.log(dbArticle)
+    res.render('index', { articles: dbArticle });
     });
-})
-
-app.get("/articles", (req, res) => {
-    db.Article.find({}).then(dbArticle => {
-        res.json(dbArticle)
-    }).catch(err => {
-        res.json(err)
-    })
-})
-// Make a request via axios to grab the HTML body from the site of your choice
-axios.get("https://bleacherreport.com/featured/").then(function(response) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("li.featuredImage").each(function(i, element) {
-
-    var link = $(element).find('div.articleMedia').find('a').attr('href');
-    // var link = $(element).find("a").attr("href");
-    var articleTitle = $(element).find('div.commentary').find('h3').text();
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      articleTitle: articleTitle,
-      link: link
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
 });
 
-app.listen(PORT, function () {
+// Make a request via axios to grab the HTML body from the site of your choice
+app.get('/scrape', (req, res) => {
+    
+    axios.get('https://bleacherreport.com/featured/').then(response => {
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
+
+    // An empty array to save the data that we'll scrape
+    var results = [];
+
+    // Select each element in the HTML body from which you want information.
+    $('li.featuredImage').each((i, element) => {
+        var link = $(element)
+            .find('div.articleMedia')
+            .find('a')
+            .attr('href');
+        // var link = $(element).find("a").attr("href");
+        var articleTitle = $(element)
+            .find('div.commentary')
+            .find('h3')
+            .text();
+        var articleSummary = $(element)
+            .find('div.articleContent')
+            .find('p.articleDescription')
+            .text();
+        var img = $(element)
+            .find('div.articleMedia')
+            .find('a.thumbnailImage')
+            .find('img.lazyImage')
+            .attr('src');
+        // Save these results in an object that we'll push into the results array we defined earlier
+        results.push({
+            articleTitle: articleTitle,
+            link: link,
+            summary: articleSummary,
+            image: img,
+        });
+        db.Article.create(results)
+            .then(function(dbArticle) {
+                console.log(dbArticle);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        });
+        // Log the results once you've looped through each of the elements found with cheerio
+        console.log(results);
+        
+    });
+});
+
+app.post('/insert', (req, res) => {});
+
+app.post('/update', (req, res) => {});
+
+app.get('/article', (req, res) => {
+    //retrieves articles from db
+    db.Article.find(
+        {}.then(function(err, foundArt) {
+        if (err) {
+            console.log(err);
+        } else {
+            // Otherwise, send json of the articles back to user
+            // This will fire off the success function of the ajax request
+            res.json(foundArt);
+        }
+        })
+    );
+});
+
+app.listen(PORT, () => {
     console.log('App running on port ' + PORT + '!');
 });

ea67012a45a8c4558b9e4180e2b5f0dcabad68c8
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-07-05 18:10:28 -0700

    still working on getting articles to be added to saved

--- server.js
+++ server.js
@@ -31,97 +31,124 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // Routes
 app.get('/', (req, res) => {
     db.Article.find({}).then(dbArticle => {
-    // console.log(dbArticle)
-    res.render('index', { articles: dbArticle });
+        // console.log(dbArticle)
+        res.render('index', { articles: dbArticle });
     });
 });
 
 // Make a request via axios to grab the HTML body from the site of your choice
 app.get('/scrape', (req, res) => {
-    
     axios.get('https://bleacherreport.com/featured/').then(response => {
-    // Load the HTML into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-    var $ = cheerio.load(response.data);
+        // Load the HTML into cheerio and save it to a variable
+        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+        var $ = cheerio.load(response.data);
 
-    // An empty array to save the data that we'll scrape
-    
+        // An empty array to save the data that we'll scrape
 
-    // Select each element in the HTML body from which you want information.
-    $('li.featuredImage').each((i, element) => {
+        // Select each element in the HTML body from which you want information.
+        $('li.featuredImage').each((i, element) => {
+            var results = {};
+            results.link = $(element)
+                .find('div.articleMedia')
+                .find('a')
+                .attr('href');
+            // var link = $(element).find('a').attr('href');
+            results.title = $(element)
+                .find('div.commentary')
+                .find('h3')
+                .text();
+            results.summary = $(element)
+                .find('div.articleContent')
+                .find('p.articleDescription')
+                .text();
+            results.image = $(element)
+                .find('div.articleMedia')
+                .find('a.thumbnailImage')
+                .find('img.lazyImage')
+                .attr('src');
 
-        var results = {};
-        results.link = $(element)
-            .find('div.articleMedia')
-            .find('a')
-            .attr('href');
-        // var link = $(element).find('a').attr('href');
-        results.title = $(element)
-            .find('div.commentary')
-            .find('h3')
-            .text();
-        results.summary = $(element)
-            .find('div.articleContent')
-            .find('p.articleDescription')
-            .text();
-        results.image = $(element)
-            .find('div.articleMedia')
-            .find('a.thumbnailImage')
-            .find('img.lazyImage')
-            .attr('src');
-
-        db.Article.create(results)
-            .then(function(dbArticle) {
-                console.log(dbArticle);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+            db.Article.create(results)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
         });
-
-        
     });
 });
 
 app.get('/article', (req, res) => {
     //retrieves articles from db
     db.Article.find(
-        {}.then(function(err, foundArt) {
-        if (err) {
-            console.log(err);
-        } else {
-            // Otherwise, send json of the articles back to user
-            // This will fire off the success function of the ajax request
-            res.json(foundArt);
-        }
+        {}.then(function (err, foundArt) {
+            if (err) {
+                console.log(err);
+            } else {
+                // Otherwise, send json of the articles back to user
+                // This will fire off the success function of the ajax request
+                res.json(foundArt);
+            }
         })
     );
 });
 
-
-app.post('/articles/:id', function(req, res) {
+app.post('/articles/:id', function (req, res) {
     // TODO
     // ====
     // save the new note that gets posted to the Notes collection
     // then find an article from the req.params.id
     // and update it's 'note' property with the _id of the new note
     db.Comment.create(req.body)
-    .then(function(dbNote) {
-        return db.Article.findOneAndUpdate({}, { $push: { notes: dbComment._id } }, { new: true });
-    }).then(function(dbArticle) {
-        res.json(dbArticle)
-    })
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate(
+                {},
+                { $push: { notes: dbComment._id } },
+                { new: true }
+            );
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        });
 });
 
-app.get("/saved", function (req, res) {
-    db.Article.find({saved: true})
-        .then(function(savedArticles) {
-            res.render("saved", {articles: savedArticles});
+app.get('/saved', function (req, res) {
+    db.Article.find({ saved: true })
+        .then(function (savedArticles) {
+            res.render('saved', { articles: savedArticles });
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
         });
 });
+
+app.post('/articles/:id', function (req, res) {
+    db.Comment.create(req.body).then(dbComment => {
+        db.Article.findOneAndUpdate(
+            {
+                _id: req.params.id,
+            },
+            {
+                comment: dbComment._id,
+            },
+            {
+                new: true,
+            }
+        )
+            .then(dbArticle => {
+                res.json(dbArticle);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+});
+
+app.delete("/articles/:id", function (req, res) {
+    db.Note.remove({
+        _id: req._id
+    }, res);
+})
 
 app.listen(PORT, () => {
     console.log('App running on port ' + PORT + '!');
